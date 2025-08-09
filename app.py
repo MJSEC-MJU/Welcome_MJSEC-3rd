d7ac1f856f6bbfafdb5e5d80bf8fa6aa575a926b
yunttai <yoont1016@gmail.com>
2025-08-09 09:20:01 +0900

    Update: app.py

--- app.py
+++ app.py
@@ -20,7 +20,11 @@ def handle_http_exception(e):
 
 @app.route("/")
 def index():
-    return "HTTP응답코드 중 400번대를 발생시키면, 플래그가 출력됩니다. 해당 플래그를 구글 폼에 적어주세요."
+    return '''
+HTTP응답코드 중 400번대를 발생시키면, 플래그가 출력됩니다. 해당 플래그를 구글 폼에 적어주세요.
+
+소스코드: https://github.com/MJSEC-MJU/Welcome_MJSEC-3rd
+'''
 
 
 @app.route('/login', methods=['GET', 'POST'])
